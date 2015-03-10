60ae953eb97a0d7f0faed9127dc33259c8c27874
pnisula <nisula.pekka@gmail.com>
2015-03-10 10:50:50 +0200

    UnityLesson
    First Commit

---
+++ UnityLesson/Assets/RotateModel.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateModel : MonoBehaviour {
+	private Vector3 lastMousePosition;
+	public Transform anatomyModel;
+	public GUIStyle guistyle;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButton(0))
+		{
+
+			//GameObject model = GameObject.Find("Female_119");
+			//model.SetActive(false);
+
+
+			
+
+		}
+		lastMousePosition = Input.mousePosition;
+	}
+
+	void OnGUI()
+	{
+		GUILayout.BeginArea(new Rect(10,10,Screen.width-10, Screen.height-10),"Test");
+		if(GUILayout.Button("TEst"))
+		{
+
+		}
+		GUILayout.EndArea();
+	}
+}
---
+++ UnityLesson/Assets/test.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetMouseButtonDown(0))
+		{
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if (Physics.Raycast(ray, out hit))
+			{
+				print("Hit: "+hit.collider.name);
+				//GameObject obj = GameObject.Find(hit.collider.name);
+				Color modelColor = hit.transform.renderer.material.color;
+				modelColor.a=0;
+				modelColor.r=0;
+				modelColor.g=0;
+				modelColor.b=0;
+				hit.transform.renderer.material.color = modelColor;
+			}
+		}
+	}
+}
